9b105c019aa554ef8aa8086d1e8b1f345f38341a
FazliddinFayziev <fazrez4515@gmail.com>
2023-11-15 18:52:27 +0800

    first commit

---
+++ movies/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from .models import Movie
+
+
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', { 'movies': movies })
+
+def details(request, movie_id):
+    try:
+        movie = Movie.objects.get(id=movie_id)
+        return render(request, 'movies/details.html', { 'movie': movie })
+    except Movie.DoesNotExist:
+        raise Http404()
